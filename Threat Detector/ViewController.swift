2574be0a9be988532beaad4268806c6eea922879
Ebo Sompa Dennis <esdennis01@gmail.com>
2020-11-11 20:21:16 -0500

    Initial Commit

---
+++ Threat Detector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Threat Detector
+//
+//  Created by Ebo Sompa Dennis on 11/11/20.
+//  Copyright © 2020 Ebo Sompa Dennis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
