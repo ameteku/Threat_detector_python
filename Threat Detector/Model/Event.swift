95a6ff1df28de7ebc21e41adde92c557a23c0929
Ebo Sompa Dennis <esdennis01@gmail.com>
2020-11-15 04:48:38 -0500

    TableVIew load from array

---
+++ Threat Detector/Model/Event.swift
@@ -0,0 +1,12 @@
+//
+//  Event.swift
+//  Threat Detector
+//
+//  Created by Ebo Sompa Dennis on 11/14/20.
+//  Copyright © 2020 Ebo Sompa Dennis. All rights reserved.
+//
+
+struct Event {
+    let message: String
+    let time: String
+}
